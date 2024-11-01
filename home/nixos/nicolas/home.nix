75ff998c2d35f13d8d5a72ca1bcc0ef1e6b028ab
nicolasribeiroo <git@niskii.dev>
2024-11-01 12:49:55 -0300

    organize podman packages

--- home/nixos/nicolas/home.nix
+++ home/nixos/nicolas/home.nix
@@ -22,10 +22,6 @@
       # encryption
       cryptsetup
       # development
-      dive # look into docker image layers
-      docker-compose # start group of containers for dev
-      podman-compose # start group of containers for dev
-      podman-tui # status of containers in the terminal
       vscode
       # daily
       google-chrome
--- system/shared/virtualisation.nix
+++ system/shared/virtualisation.nix
@@ -11,5 +11,9 @@
 
   environment.systemPackages = with pkgs; [
     virt-manager
+    dive # look into docker image layers
+    docker-compose # start group of containers for dev
+    podman-compose # start group of containers for dev
+    podman-tui # status of containers in the terminal
   ];
 }
